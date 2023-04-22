419b25a22d13268236b9ab9dc0ded326e58b6ec9
racuya <rororacuya@gmail.com>
2023-04-22 14:56:46 +0800

    basin mu hang napud

--- whistle/MainActivity.cs
+++ whistle/MainActivity.cs
@@ -10,17 +10,20 @@ using Toolbar = AndroidX.AppCompat.Widget.Toolbar;
 using AlertDialog = Android.App.AlertDialog;
 using Firebase.Auth;
 using whistle.AppDataHelper;
+using Org.Apache.Http.Authentication;
+using whistle.Activities;
 
 namespace whistle
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = false)]
     public class MainActivity : AppCompatActivity
     {
+        Toolbar toolbar;
+        FirebaseAuth auth;
+        datahelper appdata = new datahelper();
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            Toolbar toolbar;
-            FirebaseAuth auth;
-            datahelper appdata = new datahelper();
+            
             
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
@@ -48,13 +51,28 @@ namespace whistle
                 builder.SetTitle("Confirm!");
                 builder.SetMessage("Are you sure you want to log out?");
 
-                builder.SetPositiveButton("Yes",(sender, e) =>
+                builder.SetPositiveButton("Yes", (sender, e) =>
                 {
-                    
+                    auth.SignOut();
                     Toast.MakeText(this, "Logged out Successfully!", ToastLength.Short).Show();
-                }
+                    StartActivity(typeof(LoginActivity));
+                });
+
+                builder.SetNegativeButton("Cancel", (sender, e) =>
+                {
+                    return;
+                });
+
+                Dialog dialog = builder.Create();
+                dialog.Show();
 
             }
+
+            if (id == Resource.Id.action_refresh)
+            {
+
+            }
+            return base.OnOptionsItemSelected(item);
         }
 
     }
